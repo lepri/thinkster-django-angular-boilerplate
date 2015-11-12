4220228e86ff5da7ee5398a90c4946c23191c91d
Gustavo Lepri <gustavolepri@gmail.com>
2015-11-12 10:06:32 -0200

    versao final

--- static/javascripts/thinkster.routes.js
+++ static/javascripts/thinkster.routes.js
@@ -20,6 +20,14 @@
       controller: 'LoginController',
       controllerAs: 'vm',
       templateUrl: '/static/templates/authentication/login.html'
+    }).when('/+:username', {
+      controller: 'ProfileController',
+      controllerAs: 'vm',
+      templateUrl: '/static/templates/profiles/profile.html'
+    }).when('/+:username/settings', {
+      controller: 'ProfileSettingsController',
+      controllerAs: 'vm',
+      templateUrl: '/static/templates/profiles/settings.html'
     }).when('/' , {
       controller: 'IndexController',
       controllerAs: 'vm',
